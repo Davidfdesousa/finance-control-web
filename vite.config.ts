8bd411aaa750a07e1b2b97e5b0121bc58136f5d5
David Ferreira <davidfdesousa@hotmail.com>
2026-06-12 18:56:11 -0300

    fix: adjusts github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/finance-control-web/',
   build: {
     target: 'es2022',
+    outDir: 'docs',
+    emptyOutDir: true,
   },
 });
